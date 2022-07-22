c6885b81e90bca1e623cc80aadf8d094bcf639f8
alexandyrzph <alexandyrzph@gmail.com>
2022-07-22 12:43:48 +0300

    signin cleanup

--- pixelplace/src/components/SignIn.js/SignIn.js
+++ pixelplace/src/components/SignIn.js/SignIn.js
@@ -1,18 +1,7 @@
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
 const SignIn = () => {
   return (
     <div>
-      <Card>
-        <Card.Header as="h5">Featured</Card.Header>
-        <Card.Body>
-          <Card.Title>Special title treatment</Card.Title>
-          <Card.Text>
-            With supporting text below as a natural lead-in to additional content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
+     
     </div>
   );
 };
