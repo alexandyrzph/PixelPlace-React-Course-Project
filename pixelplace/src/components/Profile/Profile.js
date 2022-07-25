cca21e107c6adce2e2a3a640b5acd9dcee64b6e0
alexandyrzph <alexandyrzph@gmail.com>
2022-07-25 15:02:09 +0300

    profile route changed

--- pixelplace/src/components/Profile/Profile.js
+++ pixelplace/src/components/Profile/Profile.js
@@ -1,7 +1,11 @@
 const Profile = () => {
   return (
-    <div>Profile</div>
-  )
-}
+    <div>
+      <div className="flex relative z-20 justify-center lg:justify-between w-full max-w-xs sm:max-w-sm md:max-w-lg lg:max-w-3xl xl:max-w-3xl overflow-ellipsis 2xl:max-w-7xl mx-auto h-screen pt-[30%] md:pt-[10%]">
+        
+      </div>
+    </div>
+  );
+};
 
-export default Profile
+export default Profile;
