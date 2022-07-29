8a748c547321b29c566a92bca85ee73f601d4c2b
alexandyrzph <alexandyrzph@gmail.com>
2022-07-29 17:58:27 +0300

    details from db

--- pixelplace/src/pages/Posts/Posts.js
+++ pixelplace/src/pages/Posts/Posts.js
@@ -1,30 +1,28 @@
-import { PostItem } from "../../components";
-import Dots from "../../assets/26432.svg";
+import { useState } from "react";
 import { useUserAuth } from "../../context/UserAuthContext";
+import { PostItem } from "../../components";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "../../firebase";
-import { useState } from "react";
+import Dots from "../../assets/26432.svg";
 
 const Posts = () => {
     const { user } = useUserAuth();
-    const [posts, setPosts ] = useState([]);
+    const [posts, setPosts] = useState([]);
     useState(() => {
-    console.log(posts);
         getDocs(collection(db, "Posts"))
             .then((snapshot) => {
                 let posts = [];
                 snapshot.forEach((doc) => posts.push(doc.data()));
-                setPosts(prev => posts);
+                setPosts((prev) => posts);
             })
             .catch((err) => console.log(err.message));
-    }, []); 
-    console.log(posts);
-
+    }, []);
+    
     return (
         <div className="relative max-w-md px-1 md:px-0 sm:max-w-lg md:max-w-xl lg:max-w-3xl xl:max-w-5xl 2xl:max-w-7xl mx-auto w-full h-screen py-16">
             <div className="masonry sm:masonry-sm md:masonry-md lg:masonry-lg">
                 {posts.map((post) => (
-                    <PostItem key={post.uid} {...post} user={user} />
+                    <PostItem key={post.postId} {...post} user={user} />
                 ))}
             </div>
             <img
